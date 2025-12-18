9b117feb2b2680bcdfcdd204e79a9f2b3a38135f
Sa9m <abyssalflerken@gmail.com>
2025-12-18 00:50:37 -0600

    'When in doubt, drop the crate and write it yourself'- Someone smart probably

--- 2025/Eleven/src/main.rs
+++ 2025/Eleven/src/main.rs
@@ -56,8 +56,27 @@ fn first_half(test: bool) -> u128 {
 
     // First, we have to read the data (true == test, false == input)
     let raw_data: String = read_contents(test);
-    let data: Vec<String> = get_lines(&raw_data);
+    let mut data: Vec<String> = get_lines(&raw_data);
     
+    // This day is particularly annoying because the test inputs are different, so I have to parse them differently today
+    if test {
+        let mut drop = true;
+        for (i, row) in data.clone().iter().enumerate().rev() {
+            if *row == "SECOND HALF".to_string() {
+                data.remove(i);
+                drop = false;
+                continue;
+            }
+            if *row == "FIRST HALF".to_string() {
+                data.remove(i);
+                continue;
+            }
+            if drop {
+                data.remove(i);
+                continue;
+            }
+        }
+    }
     
     let mut devices = Graph::<&str, i32>::new();
     let mut nodes: HashMap<&str, NodeIndex> = HashMap::new();
@@ -82,10 +101,8 @@ fn first_half(test: bool) -> u128 {
         
     }
 
-    
     // This is so obviously a graph that I have decided it is time to learn a bit more about them
     // Today, that will be through the petgraph crate
-
     let all_paths: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
         &devices, 
         *nodes.get("you").expect("Can't find you!"), 
@@ -99,222 +116,89 @@ fn first_half(test: bool) -> u128 {
 }
 
 
-
+// I was having trouble with the crate on the second part, so I went and wrote it myself
 fn second_half(test: bool) -> u128 {
 
-    // First, we have to read the data (true == test, false == input)
     let raw_data: String = read_contents(test);
-    let data: Vec<String> = get_lines(&raw_data);
-    
-    
-    let mut devices = Graph::<&str, i32>::new();
-    let mut nodes: HashMap<&str, NodeIndex> = HashMap::new();
-    
-    // Adding in the nodes
-    for row in data.iter() {
-        let node = row.split(":").collect::<Vec<&str>>()[0];
-        nodes.insert(node, devices.add_node(node));
-    }
-    // Adding out manually
-    nodes.insert("out", devices.add_node("out"));
+    let mut data: Vec<String> = get_lines(&raw_data);
 
-
-    // Adding in the edges
-    for row in data.iter() {
-        let split_row = row.split(": ").collect::<Vec<&str>>();
-        let node = nodes.get(split_row[0]).unwrap();
-
-        for other in split_row[1].split(" ") {
-            devices.add_edge(*node, *nodes.get(other).unwrap(), 1); 
+    // This day is particularly annoying because the test inputs are different, so I have to parse them differently today
+    if test {
+        let mut drop = false;
+        for (i, row) in data.clone().iter().enumerate().rev() {
+            if *row == "SECOND HALF".to_string() {
+                drop = true;
+            }
+            if *row == "FIRST HALF".to_string() {
+                data.remove(i);
+                continue;
+            }
+            if drop {
+                data.remove(i);
+                continue;
+            }
         }
-        
     }
-    
-    // This is so obviously a graph that I have decided it is time to learn a bit more about them
-    // Today, that will be through the petgraph crate
 
-    // We should be able to find every path from svr-dac-fft-out and add it to svr
-    // let svr_dac: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("svr").expect("Can't find svr!"), 
-    //     *nodes.get("dac").expect("Can't find out"),
-    //     0,
-    //     None
-    // ).collect();
-    // let dac_fft: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("dac").expect("Can't find dac!"), 
-    //     *nodes.get("fft").expect("Can't find fft"),
-    //     0,
-    //     None
-    // ).collect();
-    // let fft_out: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("fft").expect("Can't find fft!"), 
-    //     *nodes.get("out").expect("Can't find out"),
-    //     0,
-    //     None
-    // ).collect();
-    // println!("Found {} with dac first.", svr_dac.len() * dac_fft.len() * fft_out.len());
-    // let svr_fft: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("svr").expect("Can't find svr!"), 
-    //     *nodes.get("fft").expect("Can't find fft"),
-    //     0,
-    //     None
-    // ).collect();
-    // let fft_dac: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("fft").expect("Can't find fft!"), 
-    //     *nodes.get("dac").expect("Can't find dac"),
-    //     0,
-    //     None
-    // ).collect();
-    // let dac_out: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<NodeIndex>, _, RandomState>(
-    //     &devices, 
-    //     *nodes.get("dac").expect("Can't find dac!"), 
-    //     *nodes.get("out").expect("Can't find out"),
-    //     0,
-    //     None
-    // ).collect();
+    // This time I will be loading into a hashmap for easy and fast access to each device with links based on the name (a &str)
+    let mut hashbrown: HashMap::<String, Vec<&str>> = HashMap::new();
+    for row in data.iter() {
+        let split = row.split(": ").collect::<Vec<&str>>();
+        hashbrown.insert(split[0].to_string(), split[1].split(" ").collect::<Vec<&str>>());
+    }
 
-    let dac = *nodes.get("dac").unwrap();
-    let fft = *nodes.get("fft").unwrap();
-    let svr = *nodes.get("svr").unwrap();
-    let out = *nodes.get("out").unwrap();
+    // Finding the paths that hit fft first
+    let fft_first = count_paths(&mut hashbrown, "svr", "fft") * count_paths(&mut hashbrown, "fft", "dac") * count_paths(&mut hashbrown, "dac", "out");
 
+    // Finding the paths that hit dac first
+    let dac_first = count_paths(&mut hashbrown, "svr", "dac") * count_paths(&mut hashbrown, "dac", "fft") * count_paths(&mut hashbrown, "fft", "out");
 
-
-    let total = rayon::join(
-        || {
-            if has_path_connecting(&devices, svr, dac, None) 
-                && has_path_connecting(&devices, dac, fft, None) 
-                && has_path_connecting(&devices, fft, out, None) {
-
-                let svr_dac: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, svr, dac, 0, None).collect();   
-                let dac_fft: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, dac, fft, 0, None).collect();
-                let fft_out: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, fft, out, 0, None).collect();
-                svr_dac.len() * dac_fft.len() * fft_out.len()
-
-            } else {
-                // panic!("Didn't find path with dac first");
-                0
-            }
-        },
-        || {
-            if has_path_connecting(&devices, svr, fft, None) 
-                && has_path_connecting(&devices, fft, dac, None) 
-            && has_path_connecting(&devices, dac, out, None) {
-                let svr_fft: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, svr, fft, 0, None).collect();
-                let fft_dac: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, fft, dac, 0, None).collect();
-                let dac_out: Vec<Vec<NodeIndex>> = all_simple_paths::<Vec<_>, _, RandomState>(&devices, dac, out, 0, None).collect();
-                svr_fft.len() * fft_dac.len() * dac_out.len()
-            } else {
-                // panic!("Didn't find path with fft first");
-                0
-            }
-        },
-    );
-
-    (total.0 + total.1) as u128   
-
-    // (svr_dac.len() * dac_fft.len() * fft_out.len() + svr_fft.len() * fft_dac.len() * dac_out.len()) as u128
-
-
+    dac_first + fft_first
 }
 
-fn different_second_half(test: bool) -> u128 {
-    
-    // First, we have to read the data (true == test, false == input)
-    let raw_data: String = read_contents(test);
-    let data: Vec<String> = get_lines(&raw_data);
-    
-    
-    let mut devices = Graph::<&str, i32>::new();
-    let mut nodes: HashMap<&str, NodeIndex> = HashMap::new();
-    
-    // Adding in the nodes
-    for row in data.iter() {
-        let node = row.split(":").collect::<Vec<&str>>()[0];
-        nodes.insert(node, devices.add_node(node));
-    }
-    // Adding out manually
-    nodes.insert("out", devices.add_node("out"));
-
-
-    // Adding in the edges
-    for row in data.iter() {
-        let split_row = row.split(": ").collect::<Vec<&str>>();
-        let node = nodes.get(split_row[0]).unwrap();
-
-        for other in split_row[1].split(" ") {
-            devices.add_edge(*node, *nodes.get(other).unwrap(), 1); 
+// A function to begin the recursive search so that we can memoize it
+fn count_paths(hashbrown: &mut HashMap<String, Vec<&str>>, start: &str, end: &str) -> u128{
+    let mut memo: HashMap<(String, String), u128> = HashMap::new();
+    let mut out: u128 = 0;
+    // Begin at every ending point 
+    for device in hashbrown.clone().iter() {
+        if *device.0 == start {
+            out += count_paths_recursive(&hashbrown, start.to_string(), end.to_string(), &mut memo);
         }
-        
     }
-
-    
-    // This is so obviously a graph that I have decided it is time to learn a bit more about them
-    // Today, that will be through the petgraph crate
-
-    let dac_node =  nodes.get("dac").unwrap();
-    let fft_node =  nodes.get("fft").unwrap();
-    let start_node =  nodes.get("svr").unwrap();
-    let end_node = nodes.get("out").unwrap();
-
-    
-    // I love love memoization
-    let mut memo = HashMap::new(); 
-
-    // Going fft then dac
-    let svr_to_dac = count_paths_to_target(&devices, *start_node, *dac_node, &mut memo);
-    let dac_to_fft = count_paths_to_target(&devices, *dac_node, *fft_node, &mut memo);
-    let fft_to_out = count_paths_to_target(&devices, *fft_node, *end_node, &mut memo);
-    let count_a = svr_to_dac * dac_to_fft * fft_to_out;
-    println!("a: {}", count_a);
-
-    // memo = HashMap::
-
-    // Going dac fft
-    let svr_to_fft = count_paths_to_target(&devices, *start_node, *fft_node, &mut memo);
-    let fft_to_dac = count_paths_to_target(&devices, *fft_node, *dac_node, &mut memo);
-    let dac_to_out = count_paths_to_target(&devices, *dac_node, *end_node, &mut memo);
-    let count_b = svr_to_fft * fft_to_dac * dac_to_out;
-    println!("b: {}", count_b);
-
-    count_a + count_b
-
-
+    out
 }
 
-// It was taking far too long with the old solution, so 
-fn count_paths_to_target(
-    graph: &Graph<&str, i32>,
-    start_node: NodeIndex,
-    end_node: NodeIndex,
-    memo: &mut HashMap<NodeIndex, u128>,
-) -> u128 {
-    // Base case: If we are already at the end node, there is 1 path (the current one)
-    if start_node == end_node {
+
+fn count_paths_recursive(devices: &HashMap<String, Vec<&str>>, start: String, end: String, memo: &mut HashMap<(String, String), u128>) -> u128 {
+
+    // Checking to see if we have been here before
+    let key = (start.clone(), end.clone());
+    if let Some(out) = memo.get(&key) {
+        return *out;
+    }
+
+    // The base case: we have found the end 
+    if start == end {
         return 1;
     }
+
+    // Skipping out because it doesn't have anywhere else to go (it would have already been counted if it was where we wanted to go)
+    if start == "out".to_string() {
+        return 0;
+    }
+
+    let mut out = 0u128;
     
-    // Check if we've already computed the paths from this node to the end
-    if let Some(&count) = memo.get(&start_node) {
-        return count;
-    }
+    // For every device that the start links to, we will recur
+    for device in devices.get(&start).unwrap() {
+        // println!("Made it to {} trying to get to {}", start, end);
+        out += count_paths_recursive(devices, device.to_string(), end.clone(), memo);
+    } 
 
-    let mut total_count = 0;
-    // Iterate over all neighbors (children) of the current node
-    for edge in graph.edges_directed(start_node, Outgoing) {
-        let next_node = edge.target();
-        // Recursively sum the counts from all downstream paths
-        total_count += count_paths_to_target(graph, next_node, end_node, memo);
-    }
-
-    // Memoize the result before returning
-    memo.insert(start_node, total_count);
-    total_count
+    // Memoizing this result 
+    memo.insert(key, out);
+    out
 }
 
 // Unit testing
@@ -354,6 +238,6 @@ fn second_half_check() {
     let answer: Option<u128> = SECOND_HALF_ANSWER;
     match answer {
         None => {println!("Second half test answer not given")},
-        Some(a) => { println!("Second half {}", {if a == first_half(true){"passing!"} else {"FAILING"}})},
+        Some(a) => { println!("Second half {}", {if a == second_half(true){"passing!"} else {"FAILING"}})},
     }
 }
